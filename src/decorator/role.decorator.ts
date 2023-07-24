7460dd5f769d304b690af29a29fed2e57c074a77
雷毅 <liam@douyu.tv>
2023-07-24 13:57:58 +0800

    feat: 学习Decorator

--- src/decorator/role.decorator.ts
+++ src/decorator/role.decorator.ts
@@ -3,8 +3,42 @@
  * @author 雷毅
  */
 
-import { SetMetadata } from '@nestjs/common';
+import {
+  ExecutionContext,
+  SetMetadata,
+  createParamDecorator,
+} from '@nestjs/common';
 
 export default function Role(...role: string[]) {
   return SetMetadata('role', role);
 }
+
+export const UseReqUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+
+    return req.user;
+  },
+);
+
+// 从 data 接受外部传入的参数
+// 用法：
+// findSomeOne (@UseReqUser('firstName') firstName: string) { /* ... */ }
+export const UseReqUser_2 = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    const user = req.user;
+
+    return data ? user?.[data] : user;
+  },
+);
+
+// 组合多个装饰器为一个
+// export function Auth(...roles: Role[]) {
+//   return applyDecorators(
+//     SetMetadata('roles', roles),
+//     UseGuards(AuthGuard, RolesGuard),
+//     ApiBearerAuth(),
+//     ApiUnauthorizedResponse({ description: 'Unauthorized' }),
+//   );
+// }
