062da07778a6cfb7c619a9a887350539bc6daadd
雷毅 <liam@douyu.tv>
2024-02-22 10:23:16 +0800

    feat: 项目更新

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,17 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Header,
-  Req,
-  Query,
-  Param,
-  Body,
-  HttpCode,
-  Redirect,
-  ParseIntPipe,
-  UsePipes,
-  DefaultValuePipe,
+    Controller,
+    Get,
+    Post,
+    Header,
+    Req,
+    Query,
+    Param,
+    Body,
+    HttpCode,
+    Redirect,
+    ParseIntPipe,
+    UsePipes,
+    DefaultValuePipe,
 } from '@nestjs/common';
 import { Request } from 'express';
 import IndexDto, { SubIndexDto, indexJoiSchema } from './dto/index.dto';
@@ -22,75 +22,75 @@ import ClassCheckPipe from './pipe/classCheck.pipe';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
 
-  @Get('/req')
-  getReq(@Req() req: Request) {
-    console.log('Cur Req: ', req.query);
-    return 'req route';
-  }
+    @Get('/req')
+    getReq(@Req() req: Request) {
+        console.log('Cur Req: ', req.query);
+        return 'req route';
+    }
 
-  @Get('/name')
-  getName(@Query() query): string {
-    return query.name;
-  }
+    @Get('/name')
+    getName(@Query() query): string {
+        return query.name;
+    }
 
-  @Get('/redirect')
-  @Redirect('https://www.baidu.com', 302)
-  goDirect() {
-    return {
-      url: '/',
-      statusCode: 302,
-    };
-  }
+    @Get('/redirect')
+    @Redirect('https://www.baidu.com', 302)
+    goDirect() {
+        return {
+            url: '/',
+            statusCode: 302,
+        };
+    }
 
-  // 在方法参数层面使用 Pipe（验证）
-  @Post('/name')
-  @UsePipes(new CheckPipe(indexJoiSchema))
-  @HttpCode(200)
-  @Header('My-Diy-Header', 'yep')
-  createName(@Body() body: IndexDto): IndexDto {
-    console.log('POST - /name ');
-    console.log(
-      `type - name: ${typeof body.name}、age: ${typeof body.age}、work: ${typeof body.work}`,
-    );
+    // 在方法参数层面使用 Pipe（验证）
+    @Post('/name')
+    @UsePipes(new CheckPipe(indexJoiSchema))
+    @HttpCode(200)
+    @Header('My-Diy-Header', 'yep')
+    createName(@Body() body: IndexDto): IndexDto {
+        console.log('POST - /name ');
+        console.log(
+            `type - name: ${typeof body.name}、age: ${typeof body.age}、work: ${typeof body.work}`,
+        );
 
-    return body;
-  }
+        return body;
+    }
 
-  @Post('/name2')
-  createName2(@Body(ClassCheckPipe) body: SubIndexDto): SubIndexDto {
-    console.log('POST - /name2 ');
-    console.log(
-      `type - name: ${typeof body.name}、width: ${typeof body.width}、height: ${typeof body.height}`,
-    );
+    @Post('/name2')
+    createName2(@Body(ClassCheckPipe) body: SubIndexDto): SubIndexDto {
+        console.log('POST - /name2 ');
+        console.log(
+            `type - name: ${typeof body.name}、width: ${typeof body.width}、height: ${typeof body.height}`,
+        );
 
-    return body;
-  }
+        return body;
+    }
 
-  // 在方法参数层面使用 Pipe（转换）、在转换 Pipe 前使用默认值 Pipe
-  @Get('/checkNum/:id')
-  getID(
-    @Param('id', new DefaultValuePipe(0), ParseIntPipe, NothingPipe) id,
-  ): string {
-    return `id: ${id}、type: ${typeof id}`;
-  }
-  // Ext: 除了传入类名，让 Nest 进行实例化并开启依赖注入。也可以自己实例化，一般在需要传入 options 的时候使用
-  // @Get(':id')
-  // getID(
-  //   @Param(
-  //     'id',
-  //     new ParseIntPipe({
-  //       errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
-  //     }),
-  //   )
-  //   id: number,
-  // ): string {
-  //   return `id: ${id}、type: ${typeof id}`;
-  // }
+    // 在方法参数层面使用 Pipe（转换）、在转换 Pipe 前使用默认值 Pipe
+    @Get('/checkNum/:id')
+    getID(
+        @Param('id', new DefaultValuePipe(0), ParseIntPipe, NothingPipe) id,
+    ): string {
+        return `id: ${id}、type: ${typeof id}`;
+    }
+    // Ext: 除了传入类名，让 Nest 进行实例化并开启依赖注入。也可以自己实例化，一般在需要传入 options 的时候使用
+    // @Get(':id')
+    // getID(
+    //   @Param(
+    //     'id',
+    //     new ParseIntPipe({
+    //       errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+    //     }),
+    //   )
+    //   id: number,
+    // ): string {
+    //   return `id: ${id}、type: ${typeof id}`;
+    // }
 }
--- src/people/people.controller.ts
+++ src/people/people.controller.ts
@@ -1,10 +1,10 @@
 import {
-  Controller,
-  Get,
-  SetMetadata,
-  UseFilters,
-  UseGuards,
-  UseInterceptors,
+    Controller,
+    Get,
+    SetMetadata,
+    UseFilters,
+    UseGuards,
+    UseInterceptors,
 } from '@nestjs/common';
 import { PeopleService } from './people.service';
 import { HttpExceptionFilter } from '../exception/diyHttpExceptionFilter';
@@ -17,38 +17,38 @@ import { People2Service } from './people2.service';
 @UseGuards(AuthGuard)
 @UseInterceptors(LoggerInterceptor)
 export default class PeopleController {
-  constructor(
-    private readonly peopleService: PeopleService,
-    private readonly people2Service: People2Service,
-  ) {}
+    constructor(
+        private readonly peopleService: PeopleService,
+        private readonly people2Service: People2Service,
+    ) {}
 
-  @Get()
-  getAllPeople(): string {
-    return this.peopleService.getAllPeople();
-  }
+    @Get()
+    getAllPeople(): string {
+        return this.peopleService.getAllPeople();
+    }
 
-  @Get('/another')
-  getAllPeople2(): string {
-    return this.people2Service.getPeopleNamesFromPeopleService();
-  }
+    @Get('/another')
+    getAllPeople2(): string {
+        return this.people2Service.getPeopleNamesFromPeopleService();
+    }
 
-  @Get('/error')
-  @UseFilters(HttpExceptionFilter) // 使用 自定义异常处理器
-  getError() {
-    this.peopleService.getError();
-  }
+    @Get('/error')
+    @UseFilters(HttpExceptionFilter) // 使用 自定义异常处理器
+    getError() {
+        this.peopleService.getError();
+    }
 
-  @Get('/admin')
-  // 为 handle 设置元数据。也可以使用自定义 decorator，如下 /admin_2 的例子。
-  // 可配合 Guard 的执行上下文使用
-  @SetMetadata('role', ['admin'])
-  getAdminData() {
-    this.peopleService.getAdminData();
-  }
+    @Get('/admin')
+    // 为 handle 设置元数据。也可以使用自定义 decorator，如下 /admin_2 的例子。
+    // 可配合 Guard 的执行上下文使用
+    @SetMetadata('role', ['admin'])
+    getAdminData() {
+        this.peopleService.getAdminData();
+    }
 
-  @Get('/admin_2')
-  @Role('admin')
-  getAdminData_2() {
-    this.peopleService.getAdminData();
-  }
+    @Get('/admin_2')
+    @Role('admin')
+    getAdminData_2() {
+        this.peopleService.getAdminData();
+    }
 }
