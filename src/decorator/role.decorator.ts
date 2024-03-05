53096facd40541859076602869f4162f8812f5ec
雷毅 <liam@douyu.tv>
2024-03-05 14:48:56 +0800

    feat: 项目优化

--- src/decorator/role.decorator.ts
+++ src/decorator/role.decorator.ts
@@ -4,36 +4,45 @@
  */
 
 import {
-  ExecutionContext,
-  SetMetadata,
-  createParamDecorator,
+    ExecutionContext,
+    SetMetadata,
+    createParamDecorator,
 } from '@nestjs/common';
 
 export default function Role(...role: string[]) {
-  return SetMetadata('role', role);
+    return SetMetadata('role', role);
 }
 
-export const UseReqUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
+/**
+ *  获取用户信息装饰器
+ *  data - 外部传入的参数
+ *  ctx - 执行上下文
+ */
+export const UseReqUser = createParamDecorator<unknown>(
+    (data, ctx: ExecutionContext) => {
+        const req = ctx.switchToHttp().getRequest();
 
-    return req.user;
-  },
+        return req.user;
+    },
 );
 
-// 从 data 接受外部传入的参数
-// 用法：
-// findSomeOne (@UseReqUser('firstName') firstName: string) { /* ... */ }
-export const UseReqUser_2 = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-    const user = req.user;
+/**
+ *  获取用户信息装饰器（可提取指定属性）
+ *  data - 外部传入的参数。例如：@UseReqUser_2('name') 中，data 的值为 'name'
+ *  ctx - 执行上下文
+ */
+export const UseReqUser_2 = createParamDecorator<string>(
+    (data, ctx: ExecutionContext) => {
+        const req = ctx.switchToHttp().getRequest();
+        const user = req.user;
 
-    return data ? user?.[data] : user;
-  },
+        return data ? user?.[data] : user;
+    },
 );
 
-// 组合多个装饰器为一个
+/**
+ *  组合多个装饰器为一个
+ */
 // export function Auth(...roles: Role[]) {
 //   return applyDecorators(
 //     SetMetadata('roles', roles),
